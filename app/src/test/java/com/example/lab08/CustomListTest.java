4e21abaf6f7521c172e7db2a304494a399337783
Ahmed Habib <ahmedhabib@Ahmeds-MacBook-Air.local>
2026-03-19 02:37:10 +0500

    hasdeletecity  passings

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -11,4 +11,11 @@ public class CustomListTest {
 
         assertTrue(list.hasCity(calgary));
     }
-}
+@Test
+public void testDeleteCity() {
+    CustomList list = new CustomList();
+    City city = new City("Edmonton", "AB");
+    list.addCity(city);
+    list.deleteCity(city);
+    assertFalse(list.hasCity(city));
+}}
